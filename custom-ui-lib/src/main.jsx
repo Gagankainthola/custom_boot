459aaf987eb5b27cd2d98eb2d19f0c62de4555e2
Vedant Rai <vedantrai1819@gmail.com>
2025-04-15 21:37:30 +0530

    Update the custom UI library

--- custom-ui-lib/src/main.jsx
+++ custom-ui-lib/src/main.jsx
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 import Button from "./styles/features/components/Button";
 import Card from "./styles/features/components/Card";
+import "./styles/features/colours/colours.css";
+import "./styles/features/fonts/typography.css";
+import "./styles/features/layouts/flex_layout_responsive.css";
+import "./styles/features/spacing/margin.css";
+import "./styles/features/spacing/padding.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
